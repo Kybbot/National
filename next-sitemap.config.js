100daed5f93257c897c5376a9eddd097389f42b4
Артём Одийко <odiykoartem@gmail.com>
2023-02-20 22:08:29 +0200

    Added sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+	siteUrl: "https://www.national.ua",
+	generateRobotsTxt: false,
+};
--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -10,7 +10,7 @@ type SeoProps = {
 export const Seo: FC<SeoProps> = ({
 	title = "National - українське контрактне виробництво",
 	description = "Вже більше 5 років ми займаємося контрактним виробництвом. Завдяки Нам, компанія-замовник здатна зекономити час та ресурси на виготовлення власного продукту.",
-	img = "https://national-psi.vercel.app/img/ogImage.jpg",
+	img = "https://www.national.ua/img/ogImage.jpg",
 }) => {
 	return (
 		<Head>
@@ -18,7 +18,7 @@ export const Seo: FC<SeoProps> = ({
 			<meta name="description" content={description} />
 
 			<meta property="og:type" content="website" />
-			{/* <meta property="og:url" content="" /> */}
+			<meta property="og:url" content="https://www.national.ua/" />
 			<meta property="og:title" content={title} />
 			<meta property="og:description" content={description} />
 			<meta property="og:image" content={img} />
